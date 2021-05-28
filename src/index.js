655dd89f8180ce0ebb62c078bb9ecdef8d22277b
gitmax681 <arjunmnath356@gmail.com>
2021-05-28 22:16:51 +0530

    daily work clearup

--- src/index.js
+++ src/index.js
@@ -3,25 +3,24 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import Recipes from "./components/Recipes";
 import Home from "./components/Home";
-import NotFound from "./components/NotFound"
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-} from "react-router-dom";
-import './css/index.css';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import "./css/index.css";
+import NutrientsTable from "./components/NutrientsTable";
 
+const data = {USD: {label: "dollar", quantity: 75.23432342, unit:"$"}}
 
 ReactDOM.render(
   <div id="foo">
     <Router>
       <App />
-        <Switch>
-          <Route path="/home" exact component={Home} />
-          <Route path='/notFound' exact render={()=> (<NotFound keyword="pasta"/>)}/>
-          <Route path="/search/:q" exact component={Recipes} />
-        </Switch>
-      </Router>
-    </div>,
+      <Switch>
+        <Route path="/home" exact component={Home} />
+        <Route path="/table" exact render={() => (
+        <NutrientsTable nutrients={data} />
+        )} />
+        <Route path="/search/:q" exact component={Recipes} />
+      </Switch>
+    </Router>
+  </div>,
   document.getElementById("root")
 );
